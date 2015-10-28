73222a52e906b12e327920050acf2a78e2b8184e
Marek Belisko <marek.belisko@gmail.com>
2015-10-28 21:48:39 +0100

    examples: Add io button example
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

---
+++ libs/linux/examples/io/io_example_button/io_example_button.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 MICRORISC s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <linux/types.h>
+#include <sysfs_gpio.h>
+#include <machines_def.h>
+
+#define NANO_SECOND_MULTIPLIER  1000000  // 1 millisecond = 1,000,000 Nanoseconds
+const long INTERVAL_MS = 10 * NANO_SECOND_MULTIPLIER;
+
+int main(void) {
+    int ret = 0;
+    int i = 0;
+
+    struct timespec sleepValue = {0, INTERVAL_MS};
+
+    printf("Button usage example \n");
+
+    // enable access to IOs
+    ret = setup_gpio(LED_GPIO, GPIO_DIRECTION_OUT, 0);
+    ret |= setup_gpio(BUTTON_GPIO, GPIO_DIRECTION_IN, 0);
+    if (ret)
+	    return;
+
+    // 10s loop for button testing
+    for (i = 0; i < 1000; i++) {
+	int val = value(BUTTON_GPIO);
+	if (val)
+		gpio_set_value(LED_GPIO, 1);
+	else
+		gpio_set_value(LED_GPIO, 0);
+
+        // sleep for 10ms
+        nanosleep(&sleepValue, NULL);
+    }
+
+    // finish the library
+    cleanup_gpio(LED_GPIO);
+    cleanup_gpio(BUTTON_GPIO);
+
+    return 0;
+}
---
+++ libs/linux/include/machines_def.h
@@ -0,0 +1,10 @@
+#ifndef __MACHINES_DEF_H
+#define __MACHINES_DEF_H
+
+#ifdef RPI
+#define LED_GPIO (22)
+#define BUTTON_GPIO (7)
+
+#endif /* RPI */
+
+#endif /* __MACHINES_DEF_H */ 
