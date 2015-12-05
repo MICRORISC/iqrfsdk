189fb47a696a90f6f7c7add8067b7657c38dc466
Martin Strouhal <martin.strouhal@microrisc.com>
2015-12-05 18:24:14 +0100

    Logging
    - logging improvement

--- libs/simply/simply-network-usbcdc/src/main/java/com/microrisc/simply/network/usbcdc/CDCNetworkLayer.java
+++ libs/simply/simply-network-usbcdc/src/main/java/com/microrisc/simply/network/usbcdc/CDCNetworkLayer.java
@@ -34,65 +34,65 @@ import org.slf4j.LoggerFactory;
  * Implements network layer using {@code J_CDCImpl} object.
  * <p>
  * This registers itself like an asynchronous listener of {@code J_CDCImpl}
- * object. All data comming from CDC interface is forwarder to user's registered 
- * network listener. All data designated to underlaying network are forwarded to 
+ * object. All data comming from CDC interface is forwarder to user's registered
+ * network listener. All data designated to underlaying network are forwarded to
  * J_CDCImpl's {@code J_CDCImpl} method.
  * 
  * @author Michal Konopa
  */
-public final class CDCNetworkLayer 
-extends AbstractNetworkLayer implements J_AsyncMsgListener {
+public final class CDCNetworkLayer
+        extends AbstractNetworkLayer implements J_AsyncMsgListener {
     /** Logger. */
     private static final Logger logger = LoggerFactory.getLogger(CDCNetworkLayer.class);
-    
+
     
     /** COM-port name for connection. */
     private final String portName;
-    
+
     /** Reference to CDC-object for communication. */
     private J_CDCImpl cdcImpl = null;
-    
+
     /** Registered network listener. */
     private NetworkLayerListener networkListener = null;
-    
+
     /** COM-port connection info. */
     private BaseCOMPortConnectionInfo connectionInfo=  null;
-    
+
     
     /** Frees up used resources. */
     private void freeResources() {
         cdcImpl = null;
         connectionStorage = null;
     }
-    
+
     private static NetworkConnectionStorage checkStorage(NetworkConnectionStorage storage) {
         if (storage == null) {
             throw new IllegalArgumentException("Network Connection Storage cannot "
                     + "be less null");
         }
         return storage;
-    } 
-    
+    }
+
     private static String checkPortName(String portName) {
         if ( portName == null ) {
             throw new IllegalArgumentException("Port name cannot be null");
         }
-        
+
         if ( portName.equals("") ) {
             throw new IllegalArgumentException("Port name cannot be empty string");
         }
         return portName;
-    } 
-    
+    }
+
     
     /**
-     * Creates CDC network layer object. 
+     * Creates CDC network layer object.
      * @param connectionStorage storage of network COM-port connections
      * @param portName COM-port name for communication
      * @throws com.microrisc.cdc.J_CDCImplException if some exception has occured
      *         during creating of CDC network layer 
      */
-    public CDCNetworkLayer(NetworkConnectionStorage connectionStorage, String portName) 
+    public CDCNetworkLayer(NetworkConnectionStorage connectionStorage, String portName)
             throws J_CDCImplException, Exception 
     {
         super(checkStorage(connectionStorage));
@@ -100,22 +100,22 @@ extends AbstractNetworkLayer implements J_AsyncMsgListener {
         this.cdcImpl = new J_CDCImpl(portName);
         this.connectionInfo = new BaseCOMPortConnectionInfo(portName);
     }
-    
+
    
     @Override
     public void registerListener(NetworkLayerListener listener) {
         this.networkListener = listener;
         logger.info("Listener registered");
     }
-    
+
     @Override
     public void unregisterListener() {
         cdcImpl.unregisterAsyncListener();
         networkListener = null;
-        
+
         logger.info("Listener unregistered");
     }
-    
+
     /**
      * Starts receiving data from CDC interface.
      * This methods last at least for 5 seconds due to reseting connected USB device.
@@ -123,7 +123,7 @@ extends AbstractNetworkLayer implements J_AsyncMsgListener {
     @Override
     public void start() throws NetworkLayerException {
         logger.debug("startReceivingData - start:");
-        
+
         /*
         final long RESET_DELAY = 5000;
         final long MARGIN_DELAY = 5000;
@@ -137,37 +137,37 @@ extends AbstractNetworkLayer implements J_AsyncMsgListener {
             logger.error("Cannot reset USB device: " + ex.getMessage());
             throw new NetworkLayerException(ex);
         }
-        */
+         */
         // register this as a listener of asynchronous messages from network interface
         this.cdcImpl.registerAsyncListener(this);
-        
+
         logger.debug("startReceivingData - end");
     }
-    
+
     @Override
     public void sendData(NetworkData networkData) throws NetworkLayerException {
         logger.debug("sendData - start: networkData={}", networkData);
-        
+
         // get connection info for specified request
         AbstractNetworkConnectionInfo connInfo = connectionStorage.getNetworkConnectionInfo(
                 networkData.getNetworkId()
         );
-        
+
         // no connection info
         if ( connInfo == null ) {
             throw new NetworkLayerException(
                     "No connection info for network: " + networkData.getNetworkId()
             );
         }
-        
+
         // check, if connection infos are equals
         if ( !(this.connectionInfo.equals(connInfo)) ) {
-            throw new NetworkLayerException("Connection info mismatch." 
+            throw new NetworkLayerException("Connection info mismatch."
                     + "Incomming: " + connInfo
                     + ", required: " + this.connectionInfo
             );
         }
-        
+
         try {
             // doesn't care about network ID - sends all data to COM-port set
             // in the constructor
@@ -175,28 +175,39 @@ extends AbstractNetworkLayer implements J_AsyncMsgListener {
         } catch ( Exception ex ) {
             throw new NetworkLayerException(ex);
         }
-        
+
         logger.debug("sendData - end");
     }
-    
+
     @Override
     public void destroy() {
         logger.debug("destroy - start: ");
-        
+
         cdcImpl.unregisterAsyncListener();
         freeResources();
-        
+
         logger.info("Destroyed");
         logger.debug("destroy - end");
     }
-    
+
     @Override
     public void onGetMessage(short[] data) {
-        logger.debug("onGetMessage - start: data={}", data);
-        
+        // converts to hex for print in log
+        if (logger.isDebugEnabled()) {
+            String dataString = "[";
+            for (int i = 0; i < data.length; i++) {
+                if (i == data.length - 1) {
+                    dataString += "0x " + Integer.toHexString(data[i]).toUpperCase() + "]";
+                } else {
+                    dataString += "0x " + Integer.toHexString(data[i]).toUpperCase() + ", ";
+                }
+            }
+            logger.debug("onGetMessage - start: data={}", dataString);
+        }
+
         String networkId = connectionStorage.getNetworkId(connectionInfo);
         networkListener.onGetData(new BaseNetworkData(data, networkId));
-        
+
         logger.debug("onGetMessage - end");
     }
 }
