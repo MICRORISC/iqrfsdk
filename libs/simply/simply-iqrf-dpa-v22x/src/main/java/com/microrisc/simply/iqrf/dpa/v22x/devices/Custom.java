ac6c325d058369f2a4dcd36dc4793b10e5612a01
Martin Strouhal <martin.strouhal@microrisc.com>
2016-02-11 11:11:48 +0100

    Custom peripheral
    - fixed method getDPA_AdditionalInfo

--- libs/simply/simply-iqrf-dpa-v22x/src/main/java/com/microrisc/simply/iqrf/dpa/v22x/devices/Custom.java
+++ libs/simply/simply-iqrf-dpa-v22x/src/main/java/com/microrisc/simply/iqrf/dpa/v22x/devices/Custom.java
@@ -19,7 +19,7 @@ import com.microrisc.simply.DeviceInterface;
 import com.microrisc.simply.DeviceInterfaceMethodId;
 import com.microrisc.simply.di_services.GenericAsyncCallable;
 import com.microrisc.simply.di_services.MethodIdTransformer;
-import com.microrisc.simply.iqrf.dpa.di_services.DPA_StandardServices;
+import com.microrisc.simply.iqrf.dpa.v22x.di_services.DPA_StandardServices;
 import java.util.UUID;
 
 /**
