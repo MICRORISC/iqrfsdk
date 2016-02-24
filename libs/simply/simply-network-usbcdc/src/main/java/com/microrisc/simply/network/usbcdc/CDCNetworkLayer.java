d645d25b076ae57cf378ebba6a36282670b5c9c1
Rostislav Spinar <rostislav.spinar@microrisc.com>
2016-02-24 18:17:11 +0100

    Improved tx logic on network layers
    -checking if the message has been sent correctly
    -improve logging to comply with iqrf ide

--- libs/simply/simply-network-usbcdc/src/main/java/com/microrisc/simply/network/usbcdc/CDCNetworkLayer.java
+++ libs/simply/simply-network-usbcdc/src/main/java/com/microrisc/simply/network/usbcdc/CDCNetworkLayer.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2014 MICRORISC s.r.o.
+ * Copyright 2016 MICRORISC s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,8 @@ package com.microrisc.simply.network.usbcdc;
 import com.microrisc.cdc.J_AsyncMsgListener;
 import com.microrisc.cdc.J_CDCImpl;
 import com.microrisc.cdc.J_CDCImplException;
+import com.microrisc.cdc.J_CDCSendException;
+import com.microrisc.cdc.J_DSResponse;
 import com.microrisc.simply.NetworkData;
 import com.microrisc.simply.NetworkLayerListener;
 import com.microrisc.simply.network.AbstractNetworkConnectionInfo;
@@ -39,6 +41,7 @@ import org.slf4j.LoggerFactory;
  * J_CDCImpl's {@code J_CDCImpl} method.
  * 
  * @author Michal Konopa
+ * @author Rostislav Spinar
  */
 public final class CDCNetworkLayer
         extends AbstractNetworkLayer implements J_AsyncMsgListener {
@@ -57,6 +60,16 @@ public final class CDCNetworkLayer
 
     /** COM-port connection info. */
     private BaseCOMPortConnectionInfo connectionInfo=  null;
+    
+    /**
+     * Default maximal number of CDC retries.
+     */
+    public static int MAX_CDC_STATUS_RETRIES = 3;
+
+    /**
+     * Maximal number of CDC retries.
+     */
+    private int maxCdcStatusRetries;
 
     
     /** Frees up used resources. */
@@ -90,7 +103,7 @@ public final class CDCNetworkLayer
      * @param connectionStorage storage of network COM-port connections
      * @param portName COM-port name for communication
      * @throws com.microrisc.cdc.J_CDCImplException if some exception has occured
-     *         during creating of CDC network layer 
+     *         during creating of CDC network layer
      */
     public CDCNetworkLayer(NetworkConnectionStorage connectionStorage, String portName)
             throws J_CDCImplException, Exception 
@@ -99,8 +112,8 @@ public final class CDCNetworkLayer
         this.portName = checkPortName(portName);
         this.cdcImpl = new J_CDCImpl(portName);
         this.connectionInfo = new BaseCOMPortConnectionInfo(portName);
+        this.maxCdcStatusRetries = MAX_CDC_STATUS_RETRIES;
     }
-
    
     @Override
     public void registerListener(NetworkLayerListener listener) {
@@ -118,26 +131,11 @@ public final class CDCNetworkLayer
 
     /**
      * Starts receiving data from CDC interface.
-     * This methods last at least for 5 seconds due to reseting connected USB device.
      */
     @Override
     public void start() throws NetworkLayerException {
         logger.debug("startReceivingData - start:");
 
-        /*
-        final long RESET_DELAY = 5000;
-        final long MARGIN_DELAY = 5000;
-        
-        try {
-            // reseting GW 
-            // 5 s after receiving of this command USB device is reset - program must deal with this !!!
-            cdcImpl.resetUSBDevice();
-            Thread.sleep(RESET_DELAY + MARGIN_DELAY);
-        } catch (Exception ex) {
-            logger.error("Cannot reset USB device: " + ex.getMessage());
-            throw new NetworkLayerException(ex);
-        }
-         */
         // register this as a listener of asynchronous messages from network interface
         this.cdcImpl.registerAsyncListener(this);
 
@@ -146,7 +144,7 @@ public final class CDCNetworkLayer
 
     @Override
     public void sendData(NetworkData networkData) throws NetworkLayerException {
-        logger.debug("sendData - start: networkData={}", networkData);
+        logger.debug("sendData - start: networkData: netId={}, netData={}", networkData.getNetworkId(), convertDataForLog(networkData.getData()));
 
         // get connection info for specified request
         AbstractNetworkConnectionInfo connInfo = connectionStorage.getNetworkConnectionInfo(
@@ -169,9 +167,31 @@ public final class CDCNetworkLayer
         }
 
         try {
-            // doesn't care about network ID - sends all data to COM-port set
-            // in the constructor
-            cdcImpl.sendData(networkData.getData());
+            
+            boolean dataSent = false;
+            int attempt = 0;
+
+            while (attempt++ < maxCdcStatusRetries) {
+                // have some space before sending another request
+                Thread.sleep(50);
+
+                // doesn't care about network ID - sends all data to COM-port set
+                // in the constructor
+                J_DSResponse response = cdcImpl.sendData(networkData.getData());
+                logger.info("Writing thread CDC response: {}", response.getRespValue());
+
+                if (response.getRespValue() == J_DSResponse.OK.getRespValue()) {
+                    logger.info("Data successfully sent to CDC");
+                    dataSent = true;
+                    break;
+                } else {
+                    logger.info("Data not sent to CDC, gateway is in state {}, retries {} ", response.getRespValue(), attempt);
+                }
+            }
+
+            if (!dataSent) {
+                throw new NetworkLayerException(new J_CDCSendException("Data has not been sent to the module!"));
+            }
         } catch ( Exception ex ) {
             throw new NetworkLayerException(ex);
         }
@@ -192,22 +212,34 @@ public final class CDCNetworkLayer
 
     @Override
     public void onGetMessage(short[] data) {
-        // converts to hex for print in log
-        if (logger.isDebugEnabled()) {
-            String dataString = "[";
-            for (int i = 0; i < data.length; i++) {
-                if (i == data.length - 1) {
-                    dataString += "0x " + Integer.toHexString(data[i]).toUpperCase() + "]";
-                } else {
-                    dataString += "0x " + Integer.toHexString(data[i]).toUpperCase() + ", ";
-                }
-            }
-            logger.debug("onGetMessage - start: data={}", dataString);
-        }
-
+            
+        logger.debug("onGetMessage - start: data={}", convertDataForLog(data));
+        
         String networkId = connectionStorage.getNetworkId(connectionInfo);
         networkListener.onGetData(new BaseNetworkData(data, networkId));
 
         logger.debug("onGetMessage - end");
+    }
+    
+    // coverts data to hex values for log
+    private String convertDataForLog(short[] data) {
+        
+        String dataString = null;
+
+        // converts to hex for print in log
+        if (logger.isDebugEnabled()) {
+            
+            dataString = "[";
+            
+            for (int i = 0; i < data.length; i++) {
+                if (i != data.length - 1) {
+                    dataString += String.format("%02X", data[i]) + ".";
+                } else {
+                    dataString += String.format("%02X", data[i]) + "]";
+                }
+            }
+        }
+        
+        return dataString;
     }
 }
