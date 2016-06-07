cd49e850c38f27ef7696835e39bbe26055f8db88
Marek Belisko <marek.belisko@gmail.com>
2016-06-07 20:59:08 +0200

    build.sh: Add possibility to cros_compile
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

--- libs/linux/build.sh
+++ libs/linux/build.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
-BUILD_DIR_NAME=".build"
+MACHINE="$1"
 BIN_DIR_NAME="`pwd`/bin/usr"
 
-MACHINE="RPI"
+# if machine is empty defaukt is RPI
+if [ "x$1" = "x" ]; then
+	MACHINE="RPI"
+fi
+
+BUILD_DIR_NAME=".build-$MACHINE"
 
 if [ ! -d "$BUILD_DIR_NAME" ]; then
 	mkdir "$BUILD_DIR_NAME"
 fi
 
 pushd "$BUILD_DIR_NAME"
-cmake -DCMAKE_TOOLCHAIN_FILE=../RPI-cross.cmake ../ -DCMAKE_INSTALL_PREFIX:PATH="$BIN_DIR_NAME" -DMACHINE="${MACHINE}" || exit 1
+if [ "$MACHINE" = "RPI" ]; then
+	cmake -DCMAKE_TOOLCHAIN_FILE=../RPI-cross.cmake ../ -DCMAKE_INSTALL_PREFIX:PATH="$BIN_DIR_NAME" -DMACHINE="${MACHINE}" || exit 1
+else
+# build for x86
+	cmake ../ || exit 1
+fi
 
 # build
 make -j4 || exit 1
 
 # clear output
-rm -r "$BIN_DIR_NAME"
+rm -r "$BIN_DIR_NAME_${MACHINE}"
 
 # install binaries and libs
 make install
